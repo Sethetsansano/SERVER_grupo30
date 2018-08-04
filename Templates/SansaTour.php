a90f675faa48de864c30fed67f63329c8d702f8c
sebastian.groselj@sansano.usm.cl <sebastian.groselj@sansano.usm.cl>
2018-08-04 18:18:39 -0400

    Look in console: POST

--- Templates/SansaTour.php
+++ Templates/SansaTour.php
@@ -3,11 +3,13 @@
   CallStyle();
   SelectPage();
 
+
+
   /* codigotemporal
   include "frontend.html";
   CallStyle("StyleCSS/frontend_style_boostrap.css");
-  include "frontend_style.css";
+  include "frontend_style.css";*/
 
 
-  */
+  LookPost();
 ?>
